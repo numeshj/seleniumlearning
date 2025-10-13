516a07593bac474f5c8cd4a0cb7051cf2593e16c
numeshj <numeshjayamanne@gmail.com>
2025-10-13 17:33:49 +0530

    selenium-maven project

--- src/main/java/com/example/seleniumlearning/SeleniumlearningApplication.java
+++ src/main/java/com/example/seleniumlearning/SeleniumlearningApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SeleniumlearningApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SeleniumlearningApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SeleniumlearningApplication.class, args);
+    }
 
 }
