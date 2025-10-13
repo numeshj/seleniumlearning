9ce2f8f4fb7309858fe8160adce698eb8d9e2984
numeshj <numeshjayamanne@gmail.com>
2025-10-13 17:32:23 +0530

    Initial commit

---
+++ src/main/java/com/example/seleniumlearning/SeleniumlearningApplication.java
@@ -0,0 +1,13 @@
+package com.example.seleniumlearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeleniumlearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeleniumlearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/seleniumlearning/SeleniumlearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.seleniumlearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeleniumlearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
